b592f6326981536d63f053c5e7f297d740e08949
Viresh Soedhwa <vireshsoedhwa@gmail.com>
2022-09-24 10:00:31 -0700

    add helmchart

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,14 +2,6 @@
 
 set -e
 
-if [ -z "${DJANGO_SECRET_KEY}" ];then
-  echo DJANGO_SECRET_KEY=$DJANGO_SECRET_KEY >> .env
-fi
-
-if [ -z "${GO_PIPELINE_LABEL}" ];then
-  echo GO_PIPELINE_LABEL=$GO_PIPELINE_LABEL >> .env
-fi
-
 >&2 echo "Make Database migrations"
 python manage.py makemigrations youtube
 echo "-------------------------------------------------------------------------------------------\n"
--- playlistenerweb/settings.py
+++ playlistenerweb/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         "USER": os.environ["POSTGRES_USER"],
         "PASSWORD": os.environ["POSTGRES_PASSWORD"],
         "HOST": os.environ["POSTGRES_HOST"],
-        "PORT": int(os.environ["POSTGRES_PORT"]),
+        "PORT": 5432
     }
 }
 
