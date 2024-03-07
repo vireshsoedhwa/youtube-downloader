5ae35cc6642426f7b4cc945f99ff67acab395fb6
vireshsoedhwa <vireshsoedhwa@gmail.com>
2024-03-06 22:56:27 -0800

    update detail page

--- app/src/components/Create.jsx
+++ app/src/components/Create.jsx
@@ -1,65 +1,56 @@
 import React, { Fragment, useEffect, useState, useRef } from 'react';
-import Preview from './Preview';
-import Infopanel from './Infopanel';
-import { useInterval } from './helper';
+
+import { Navigate } from "react-router-dom";
 
 export default function Create() {
 
     const [Error, setError] = useState(null)
-    const [Status, setStatus] = useState("NEW")
-    const [Id, setId] = useState(null)
-    const [Youtube_id, setYoutube_id] = useState(null)
     const [Result, setResult] = useState(null)
-    const [Pollingdelay, setPollingdelay] = useState(null)
-
-    useInterval(async () => {
-        getresult()
-    }, Pollingdelay);
+    const [Submitted, setSubmitted] = useState(false)
 
 
-    useEffect(() => {
-        if (Result) {
-            let resultstatus = Result['status']
-            if (resultstatus == "QUEUED" || resultstatus == "BUSY") {
-                setPollingdelay(1000)
-            }
-            else {
-                setPollingdelay(null)
-            }
-            setYoutube_id(Result['youtube_id'])
-            setId(Result['id'])
-            setStatus(resultstatus)
-            setError(Result['error'])
+    // useEffect(() => {
+    //     if (Result) {
+    //         let resultstatus = Result['status']
+    //         if (resultstatus == "QUEUED" || resultstatus == "BUSY") {
+    //             setPollingdelay(1000)
+    //         }
+    //         else {
+    //             setPollingdelay(null)
+    //         }
+    //         setYoutube_id(Result['youtube_id'])
+    //         setId(Result['id'])
+    //         setStatus(resultstatus)
+    //         setError(Result['error'])
 
-        }
-    }, [Result]);
+    //     }
+    // }, [Result]);
 
-    const getresult = () => {
-        let headers = new Headers()
-        headers.append("X-CSRFTOKEN", document.querySelector('[name=csrfmiddlewaretoken]').value)
-        const request = new Request('/api/resource/' + Id + '/getresult', {
-            method: 'GET',
-            headers: headers
-        });
+    // const getresult = () => {
+    //     let headers = new Headers()
+    //     headers.append("X-CSRFTOKEN", document.querySelector('[name=csrfmiddlewaretoken]').value)
+    //     const request = new Request('/api/resource/' + Id + '/getresult', {
+    //         method: 'GET',
+    //         headers: headers
+    //     });
 
-        fetch(request)
-            .then(async (response) => {
-                if (response.ok) {
-                    const jsonResponse = await response.json();
-                    setResult(jsonResponse)
-                    setError(null)
-                }
-                else {
-                    const jsonResponse = await response.json();
-                    try {
-                        setError(jsonResponse)
-                    } catch {
-                        setError("Unknown error")
-                    }
-                }
-            })
-
-    }
+    //     fetch(request)
+    //         .then(async (response) => {
+    //             if (response.ok) {
+    //                 const jsonResponse = await response.json();
+    //                 setResult(jsonResponse)
+    //                 setError(null)
+    //             }
+    //             else {
+    //                 const jsonResponse = await response.json();
+    //                 try {
+    //                     setError(jsonResponse)
+    //                 } catch {
+    //                     setError("Unknown error")
+    //                 }
+    //             }
+    //         })
+    // }
 
     const create = (url) => {
         let headers = new Headers()
@@ -78,6 +69,7 @@ export default function Create() {
                 if (response.ok) {
                     const jsonResponse = await response.json();
                     setResult(jsonResponse)
+                    setSubmitted(true)
                     setError(null)
                 }
                 else {
@@ -97,75 +89,78 @@ export default function Create() {
         create(url)
     };
 
-    const handleReset = () => {
-        setStatus("NEW")
-    };
+    // const handleReset = () => {
+    //     setStatus("NEW")
+    // };
 
-    const SUBMISSION_STATES = {
-        NEW:
-            <>
-                <button type="submit"
-                    class="text-white bg-blue-700 hover:bg-blue-800 
-                focus:ring-4 focus:outline-none focus:ring-blue-300 
-                font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 
-                text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Submit
-                </button>
-            </>,
-        QUEUED:
-            <div class="font-sans">
-                QUEUED
-                <svg class="animate-spin ml-1 mr-3 h-fit w-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                    <path class="opacity-75" fill="blue" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                </svg>
-            </div>,
-        BUSY:
-            <div>
-                <Preview youtube_id={Youtube_id} />
-                <Infopanel result={Result} />
-            </div>,
-        DONE:
-            <>
-                <Preview youtube_id={Youtube_id} />
-                <div class="flex flex-col justify-around gap-4">
-                    <div class="flex flex-row justify-evenly">
-                        <a
-                            href={"/api/resource/" + Id + "/getvideo"}>
-                            Download Video
-                            <svg xmlns="http://www.w3.org/2000/svg"
-                                fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor"
-                                class="w-6 h-6 m-auto">
-                                <path stroke-linecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
-                            </svg>
-                        </a>
-                        <a href={"/api/resource/" + Id + "/getaudio"}>
-                            Download Audio
-                            <svg xmlns="http://www.w3.org/2000/svg"
-                                fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor"
-                                class="w-6 h-6 m-auto">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z" />
-                            </svg>
-                        </a>
-                    </div>
-                    <button type="reset"
-                        class="text-white bg-blue-700 hover:bg-blue-800 
-                    focus:ring-4 focus:outline-none focus:ring-blue-300 
-                    font-medium rounded-lg text-sm w-full sm:w-auto
-                    px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 
-                    dark:focus:ring-blue-800">
-                        Reset
-                    </button>
-                </div>
-            </>,
-    }
+    // const SUBMISSION_STATES = {
+    //     NEW:
+    //         <>
+    //             <button type="submit"
+    //                 class="text-white bg-blue-700 hover:bg-blue-800 
+    //             focus:ring-4 focus:outline-none focus:ring-blue-300 
+    //             font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 
+    //             text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+    //                 Submit
+    //             </button>
+    //         </>,
+    //     QUEUED:
+    //         <div class="font-sans">
+    //             QUEUED
+    //             <svg class="animate-spin ml-1 mr-3 h-fit w-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+    //                 <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+    //                 <path class="opacity-75" fill="blue" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+    //             </svg>
+    //         </div>,
+    //     BUSY:
+    //         <div>
+    //             <Preview youtube_id={Youtube_id} />
+    //             <Infopanel result={Result} />
+    //         </div>,
+    //     DONE:
+    //         <>
+    //             <Preview youtube_id={Youtube_id} />
+    //             <div class="flex flex-col justify-around gap-4">
+    //                 <div class="flex flex-row justify-evenly">
+    //                     <a
+    //                         href={"/api/resource/" + Id + "/getvideo"}>
+    //                         Download Video
+    //                         <svg xmlns="http://www.w3.org/2000/svg"
+    //                             fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+    //                             stroke="currentColor"
+    //                             class="w-6 h-6 m-auto">
+    //                             <path stroke-linecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
+    //                         </svg>
+    //                     </a>
+    //                     <a href={"/api/resource/" + Id + "/getaudio"}>
+    //                         Download Audio
+    //                         <svg xmlns="http://www.w3.org/2000/svg"
+    //                             fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+    //                             stroke="currentColor"
+    //                             class="w-6 h-6 m-auto">
+    //                             <path stroke-linecap="round" stroke-linejoin="round" d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z" />
+    //                         </svg>
+    //                     </a>
+    //                 </div>
+    //                 <button type="reset"
+    //                     class="text-white bg-blue-700 hover:bg-blue-800 
+    //                 focus:ring-4 focus:outline-none focus:ring-blue-300 
+    //                 font-medium rounded-lg text-sm w-full sm:w-auto
+    //                 px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 
+    //                 dark:focus:ring-blue-800">
+    //                     Reset
+    //                 </button>
+    //             </div>
+    //         </>,
+    // }
 
     return (
         <>
             <div class="container mx-auto">
-                <form onSubmit={handleSubmit} onReset={handleReset}>
+                {Submitted && (
+                    <Navigate to={"/detail/" + Result['id']} replace={true} />
+                )}
+                <form onSubmit={handleSubmit} >
                     <div class="mb-6">
                         <label for="url"
                             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -178,9 +173,17 @@ export default function Create() {
                             {Error}
                         </div>
                     }
-                    {
+                    <button type="submit"
+                        class="text-white bg-blue-700 hover:bg-blue-800 
+                            focus:ring-4 focus:outline-none focus:ring-blue-300 
+                            font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 
+                            text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Submit
+                    </button>
+
+                    {/* {
                         SUBMISSION_STATES[Status]
-                    }
+                    } */}
                 </form>
             </div>
         </>
--- app/src/components/Detail.jsx
+++ app/src/components/Detail.jsx
@@ -1,15 +1,38 @@
 import React, { Fragment, useEffect, useState, useRef } from 'react';
 import { useParams } from 'react-router-dom'
 
+import Infopanel from './Infopanel';
+import { useInterval } from './helper';
+
 export default function Detail() {
     let { id } = useParams();
     const [Detail, setDetail] = useState("");
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+    const [Busy, setBusy] = useState(true);
+    const [Error, setError] = useState(null);
+    const [Pollingdelay, setPollingdelay] = useState(null)
+
+    useInterval(async () => {
+        fetchDetail()
+    }, Pollingdelay);
+
 
     useEffect(() => {
         fetchDetail()
     }, []);
+
+    useEffect(() => {
+        if (Detail.status == "QUEUED" || Detail.status == "BUSY") {
+            setPollingdelay(2000)
+            setBusy(true)
+        }
+        else {
+            setBusy(false)
+            setPollingdelay(null)
+        }
+    }, [Detail]);
+
+
+
 
     const fetchDetail = async () => {
         try {
@@ -29,7 +52,7 @@ export default function Detail() {
         } catch (error) {
             setError(error);
         } finally {
-            setLoading(false);
+            setBusy(true);
         }
     };
 
@@ -38,28 +61,40 @@ export default function Detail() {
         <div class="container mx-auto m-5">
 
             <div class="flex flex-col justify-around gap-4">
-            <h1 class="text-center">Beloved Manhattan soup stand closes</h1>
-                <div class="flex flex-row justify-evenly">
-                    <a
-                        href={"/api/resource/" + Detail.id + "/getvideo"}>
-                        Download Video
-                        <svg xmlns="http://www.w3.org/2000/svg"
-                            fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor"
-                            class="w-6 h-6 m-auto">
-                            <path stroke-linecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
-                        </svg>
-                    </a>
-                    <a href={"/api/resource/" + Detail.id + "/getaudio"}>
-                        Download Audio
-                        <svg xmlns="http://www.w3.org/2000/svg"
-                            fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor"
-                            class="w-6 h-6 m-auto">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z" />
-                        </svg>
-                    </a>
-                </div>
+                <h1 class="text-center">{Detail.title}</h1>
+                {Busy ?
+                    <>
+                        <Infopanel result={Detail} />
+                    </>
+                    :
+                    <div class="flex flex-row justify-evenly">
+                        <a
+                            href={"/api/resource/" + Detail.id + "/getvideo"}>
+                            Download Video
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor"
+                                class="w-6 h-6 m-auto">
+                                <path stroke-linecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
+                            </svg>
+                        </a>
+                        <a href={"/api/resource/" + Detail.id + "/getaudio"}>
+                            Download Audio
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor"
+                                class="w-6 h-6 m-auto">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z" />
+                            </svg>
+                        </a>
+                    </div>
+                }
+                {Error &&
+                    <div class="font-sans text-red-700 italic mb-4">
+                        {Error}
+                    </div>
+                }
+
                 <img class="object-contain"
                     src={"https://img.youtube.com/vi/" + Detail.youtube_id + "/sddefault.jpg"}
                 />
