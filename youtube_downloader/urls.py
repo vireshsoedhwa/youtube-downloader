9484a7407c63bc072423465d9ea424610280bf8f
vireshsoedhwa <vireshsoedhwa@gmail.com>
2024-03-09 03:55:10 -0800

    fresh migration

--- youtube_downloader/urls.py
+++ youtube_downloader/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
-    path('', include('app.urls')),
-    path('admin/', admin.site.urls),
+    path('', include('app.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += path('admin/', admin.site.urls)
