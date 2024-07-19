f00038a1f989aded982e29d2bd27937ef00f7b2d
vireshsoedhwa <vireshsoedhwa@gmail.com>
2024-07-19 01:31:06 -0700

    home list

--- frontend/src/containers/BaseLayout.jsx
+++ frontend/src/containers/BaseLayout.jsx
@@ -2,7 +2,6 @@ import React, { Fragment, useEffect, useState, useRef } from 'react';
 import { Outlet } from 'react-router-dom'
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
 
 export default function BaseLayout(props) {
 
@@ -10,17 +9,17 @@ export default function BaseLayout(props) {
         <Container maxWidth="sm">
             <Box
                 sx={{
-                    alignContent: "center",
-                    marginBottom: "1em",
-                    display: 'flex',
-                    justifyContent: 'space-evenly'
-
+                    // alignContent: "center",
+                    paddingTop: "1em",
+                    margin: "1em",
+                    // display: 'flex',
+                    // justifyContent: 'space-evenly'
                 }}
             >
-                <Link href="/home"> Home</Link>
-                <Link href="/create">Create</Link>
+                {/* <Link href="/home"> Home</Link>
+                <Link href="/create">Create</Link> */}
+                <Outlet />
             </Box>
-            <Outlet />
         </Container>
     )
 }
--- frontend/src/containers/Home.jsx
+++ frontend/src/containers/Home.jsx
@@ -1,9 +1,18 @@
 import React, { Fragment, useEffect, useState, useRef } from 'react';
-import { Navigate } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import AddBoxIcon from '@mui/icons-material/AddBox';
 import Button from '@mui/material/Button';
 import useResourceList from '../hooks/useResourceList'
+import Box from '@mui/material/Box';
+
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
 
 export default function Home() {
     const navigate = useNavigate();
@@ -19,13 +28,65 @@ export default function Home() {
         ResourceList()
     }, []);
 
+    const Resourcelist = () => {
+
+        return (
+            <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+                {ResourceListData.map((item, index) => (
+                    <React.Fragment key={index} >
+                        <ListItem alignItems="flex-start"
+                            onClick={() => { console.log("clicked") }}
+                        >
+                            <ListItemAvatar>
+                                <Avatar
+                                    variant="rounded"
+                                    alt="Remy Sharp"
+                                    src={"https://img.youtube.com/vi/" +
+                                        item.youtube_id +
+                                        "/sddefault.jpg"}
+                                />
+                            </ListItemAvatar>
+                            <ListItemText
+                                primary={item.title}
+                                secondary={
+                                    <React.Fragment>
+                                        <Typography
+                                            sx={{ display: 'inline' }}
+                                            component="span"
+                                            variant="body2"
+                                            color="text.primary"
+                                        >
+                                            {item.url}
+                                        </Typography>
+                                    </React.Fragment>
+                                }
+                            />
+                        </ListItem>
+                        <Divider variant="inset" component="li" />
+                    </React.Fragment>
+                ))}
+            </List>
+        );
+    }
+
     return (
-        <>
+        <Box
+            sx={{
+                alignContent: "center",
+                marginBottom: "1em",
+                display: 'flex',
+                justifyContent: 'space-evenly',
+                flexDirection: 'column'
+            }}
+        >
             <Button variant="contained"
                 onClick={() => navigate("/create")}>
                 new
                 <AddBoxIcon />
             </Button>
-        </>
+            {ResourceListIsSuccesful &&
+                <Resourcelist />
+            }
+        </Box>
     );
 }
